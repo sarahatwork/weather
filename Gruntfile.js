5c156d6aae042127ad1b233824220b21eab70ec6
Sarah Mogin <mogin@work.co>
2015-04-11 15:52:49 -0400

    Install grunt-build-control

--- Gruntfile.js
+++ Gruntfile.js
@@ -26,6 +26,21 @@ module.exports = function (grunt) {
 
     // Project settings
     yeoman: appConfig,
+    
+    buildcontrol: {
+      options: {
+        dir: 'dist',
+        commit: true,
+        push: true,
+        message: 'Built %sourceName% from commit %sourceCommit% on branch %sourceBranch%'
+      },
+      pages: {
+        options: {
+          remote: 'git@github.com:sarahatwork/weather.git',
+          branch: 'gh-pages'
+        }
+      }
+    },
 
     // Watches files for changes and runs tasks based on the changed files
     watch: {

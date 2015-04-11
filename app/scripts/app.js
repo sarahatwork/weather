63effded2e952bb014b88c7a9e3cfec96afd2ddf
Sarah Mogin <mogin@work.co>
2015-04-11 13:48:24 -0400

    initial commit - yeoman angular generator

---
+++ app/scripts/app.js
@@ -0,0 +1,31 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name weatherApp
+ * @description
+ * # weatherApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('weatherApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngRoute',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
